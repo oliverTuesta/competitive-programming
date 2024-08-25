500a485929eb58407dd0831db4da6d51f83329dc
Oliver Tuesta <oliver.jtuesta@gmail.com>
2024-08-25 18:26:14 -0500

    feat: add cp template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fst first
+#define snd second
+#define all(c) ((c).begin()), ((c).end())
+#define ll long long
+
+const int INF = 1 << 30;
+const int MAXN = 1e5+5;
+
+void solve() {
+  
+}
+
+int main() {
+  int t = 1;
+  cin >> t;
+  while(t--) 
+    solve();
+}
+
